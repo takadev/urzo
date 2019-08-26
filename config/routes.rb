26ad54741b89616b6a5456b493d9c27cb06913ad
takadev <nontan20xx@gmail.com>
2019-08-27 01:32:06 +0900

    Add todolist views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'top#index'
+  get 'todolists/new'
+  post 'todolists/create' => 'todolists#create'
+  
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
 end
